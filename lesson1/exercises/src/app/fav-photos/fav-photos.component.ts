d52338af6ea99902cd5cb5a94f66059939aa9593
Pooja Bhatia <pooja08.bhatia@gmail.com>
2021-08-17 12:02:30 -0500

    Changes in Lesson1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Look Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = 'https://www.google.com/search?q=small+images&tbm=isch&ved=2ahUKEwiy9NykmbLyAhVNfqwKHSV0BvcQ2-cCegQIABAA&oq=small+images&gs_lcp=CgNpbWcQAzIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQ6BggAEAcQHlCtgQFYgIwBYKaQAWgAcAB4AIABuAKIAaIHkgEHMS40LjAuMZgBAKABAaoBC2d3cy13aXotaW1nwAEB&sclient=img&ei=_5gYYfL4OM38sQWl6Jm4Dw#imgrc=-z3fNX8GmtnA1M';
-  image3 = 'https://www.shutterstock.com/image-photo/summer-landscape-small-lake-forest-1969595212';
+  
+  photosTitle = 'Random Images';
+   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
+   image2 = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/768px-Python-logo-notext.svg.png';
+   image3 = 'https://res.cloudinary.com/teepublic/image/private/s--V-1Uubx9--/t_Preview/b_rgb:191919,c_limit,f_jpg,h_630,q_90,w_630/v1533021249/production/designs/2959908_0.jpg';
+  
+ 
 
   constructor() { }
 
--- lesson1/exercises/src/app/page-title/page-title.component.ts
+++ lesson1/exercises/src/app/page-title/page-title.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-title.component.css']
 })
 export class PageTitleComponent implements OnInit {
-pagetitle = 'Hello World';
+title = "Angular Lesson 1 Exercises"
   constructor() { }
 
   ngOnInit() {
